9ff1e83eb58675f2710df15a304234da80c6db45
Sebastian Melchior <sebastian@melchior.me>
2018-03-27 14:11:46 +0200

    add foo2 - 2

--- global/testfoo/registries.tf
+++ global/testfoo/registries.tf
@@ -2,6 +2,6 @@ resource "aws_ecr_repository" "repository" {
   name = "footest123"
 }
 
-resource "aws_ecr_repository" "repository" {
+resource "aws_ecr_repository" "repository2" {
   name = "footest1234"
 }
