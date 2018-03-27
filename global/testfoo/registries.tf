abd63625a4276fd1e1305364e0ce75825bd11169
Sebastian Melchior <sebastian@melchior.me>
2018-03-27 14:46:43 +0200

    add user

--- global/testfoo/registries.tf
+++ global/testfoo/registries.tf
@@ -5,3 +5,11 @@ resource "aws_ecr_repository" "repository" {
 resource "aws_ecr_repository" "repository2" {
   name = "footest1234"
 }
+
+resource "aws_iam_user" "test" {
+  name = "testtfoo1"
+}
+
+resource "aws_iam_access_key" "test" {
+  user = "${aws_iam_user.test.name}"
+}
